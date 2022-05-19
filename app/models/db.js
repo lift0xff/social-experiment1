a99ec441d208a1837a51008cf3778594821e401f
lift0xff <lift0xff@outlook.com>
2022-05-19 09:15:51 +0700

    Add logging statement

--- app/models/db.js
+++ app/models/db.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import postgres from "postgres";
 dotenv.config();
-
+console.log(`PG_URI is ${process.env.PG_URI}`);
 const sql = postgres(process.env.PG_URI, {
   ssl: {
     rejectUnauthorized: false,
